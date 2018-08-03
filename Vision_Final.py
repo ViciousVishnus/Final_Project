fbdb7ca4a0e2d0d864edd7d329ea6f03d0e7ebcb
ryan11223344 <rblow@me.com>
2018-08-03 08:58:51 -0400

    Please don't adjust stuff, still working on it

--- Vision_Final.py
+++ Vision_Final.py
@@ -1,13 +1,5 @@
 
-# coding: utf-8
 
-# In[1]:
-
-
-#By Eden, Ryan, Michael, Vishnu
-
-
-# In[2]:
 from flask import Flask
 from flask_ask import session, Ask, statement, question, request
 app= Flask(__name__)
@@ -21,7 +13,7 @@ import pickle
 
 @ask.launch
 def start_skill():
-    welcome_message= 'Hello, would you like me to translate for you?'
+    welcome_message= 'Hello, could I take a picture so I can recognize you?'
     return question(welcome_message)
 
 @ask.intent("YesIntent")
@@ -29,12 +21,8 @@ def final_func():
     from camera import take_picture
     img_array = take_picture()
     import numpy as np
-
-    # download the models from dlib
     from dlib_models import download_model, download_predictor, load_dlib_models
-
     from dlib_models import models
-
     # load the models that dlib has to detect faces.
     load_dlib_models()
     face_detect2 = models["face detect"]
@@ -53,27 +41,10 @@ def final_func():
     if descriptors is []:
         return_statement="We don't see you"
         return statement(return_statement)
-    import matplotlib.pyplot as plt
     if type(detections) != list:
         detections = list(detections)
-
-    if len(detections) == 1:
-        rect = detections[0]
-        fig, ax = plt.subplots()
-        x1, x2, y1, y2 = rect.left(), rect.right(), rect.top(), rect.bottom()
-        ax.imshow(img_array[y1:y2, x1:x2, :])
-
-
-    else:
-        fig, ax = plt.subplots(ncols=len(detections))
-        # https://stackoverflow.com/questions/46615554/how-to-display-multiple-images-in-one-figure-correctly
-        for i, rect in enumerate(detections):
-            x1, x2, y1, y2 = rect.left(), rect.right(), rect.top(), rect.bottom()
-            ax[i].imshow(img_array[y1:y2, x1:x2, :])
-            ax[i].axis('off')
     names = []
     for descriptor in descriptors:
-        import numpy as np
         with open("faces.pkl", mode="rb") as opened_file:
             faces = pickle.load(opened_file)
         distances = []
